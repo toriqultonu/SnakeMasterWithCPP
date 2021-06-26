caf054ec65f1dfc2e98bc35e9aa2d9feeb91a0e0
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2021-06-26 04:04:44 -0700

    without graphics

--- SnakeMaster.cpp
+++ SnakeMaster.cpp
@@ -1,82 +1,245 @@
-#include<iostream>
-#include<graphics.h>
-#include<conio.h>
-#include<stdlib.h>
+#include <iostream>
+
+#include <conio.h>
+
 using namespace std;
-int main()
-{
- int gdriver=DETECT,gmode;
- initgraph(&gdriver,&gmode,"");
- cleardevice();
- //x and y are for the circle
- int x=300,y=250;
- //x1 and y1 are fo generating foot at random place
- int x1=200,y1=230,food=0;
- // key is used to take input from KEYBOARD
- char key;
- // a boundry line
- rectangle(100,100,500,400);
 
-// if x or y exceeds the boundry limit game is out
- while(x>100 && x<500 && y>100 && y<400)
- {
- //these are for generating food at randm pleaces..
- // these 4 conditions are if the circle that is in your ccontrol reaches the food then
- // generate new foot at new place within the boundry
- if(x+1==x1 && y+1==y1 || x-1==x1 && y+1==y1 || x+1==x1 && y-1==y1 || x-1==x1 &&
-y-1==y1)
- {
- // food counts number of dots accessed
- food++;
- againx:
- x1=rand();
- // these conditions are for generating food within the boundry
- if(x1<100 || x1>500)
- goto againx;
- againy:
- y1=rand();
- if(y1<100 || y1>400)
- goto againy;
- // this draws a dot for food
+bool gameover;
 
-circle(x1,y1,3);
- }
- // KEYS INFORMATION
- key=getch();
- if(key=='A' || key=='a')//move up
- y--;
- else if(key=='Z'||key=='z')//move right
- x++;
- else if(key=='L' || key=='l')//for left
- x--;
- else if(key=='.')//for downward
- y++;
- cleardevice();
- outtextxy(100,0,"SNAKE GAME BY JAVERIA");
- outtextxy(100,20,"Z to move right");
- outtextxy(100,40,"L to move left");
- outtextxy(100,60,". to move down");
- outtextxy(100,80,"A to move up");
- // boundry line and the circle
+const int width = 20;
 
-rectangle(100,100,500,400);
- circle(x1,y1,3);
- // you can control the circle of green color to reach to food
- setcolor(2);
- circle(x,y,4);
- setcolor(WHITE);
- }
- outtextxy(100,420,"You are out");
- cout<<"\n\n\n\tYOUR TOTAL SCORES ARE "<<food<<endl<<endl;
-//conditions so that the game exit when you press space
- char exitt;
- again:
- exitt=getch();
- if(exitt==32)
- closegraph();
- else
- goto again;
+const int height = 17;
 
-return 0;
+int x, y, fruitX, fruitY, score;
+
+int tailX[100], tailY[100]; //snake coordinates
+
+int nTail;
+
+enum eDirecton {STOP = 0, LEFT,RIGHT, UP, DOWN}; // Controls
+
+eDirecton dir;
+
+void Setup() {
+gameover = false;
+
+dir = STOP;
+
+x = width / 2;
+
+y = height / 2;
+
+fruitX = rand() % width; //display fruit in a random place
+
+fruitY = rand() % height; score = 0;
+
 }
 
+void Draw() {
+system("cls");
+
+for(int i = 0; i < width+2; i++)
+
+cout << "#";
+
+cout << endl ;
+
+for (int i = 0; i < height ; i++) {
+
+for (int j = 0; j < width; j++) {
+
+if (j == 0)
+
+cout << "#"; //walls
+
+if (i == y && j == x)
+
+cout << "*"; // snake tale
+
+else if (i == fruitY && j == fruitX )
+
+cout << "%"; // change it to change the fruit
+
+else {
+
+bool print = false;
+
+for (int k = 0; k< nTail ; k++) {
+
+if (tailX [k] == j && tailY [k] == i) {
+
+cout << "*"; print = true;
+
+}
+
+}
+
+if (!print) cout << " ";
+
+}
+
+if (j == width -1)
+
+cout << "#";
+
+}
+
+cout << endl;
+
+}
+
+for (int i = 0; i< width+2; i++)
+
+cout << "#";
+
+cout << endl;
+
+cout << "Score:" << score << endl ;
+
+}
+
+void Input ()
+{
+
+if (_kbhit ()) {
+
+switch (_getch ()) {
+
+case 'a':
+
+dir = LEFT;
+
+break;
+
+case 'd':
+
+dir = RIGHT;
+
+break;
+
+case 'w':
+
+dir = UP;
+
+break;
+
+case 's':
+
+dir = DOWN ;
+
+break;
+
+case 'x':
+
+gameover = true;
+
+break;
+
+}
+
+}
+
+}
+
+void algorithm()
+{
+
+int prevX = tailX [0];
+
+int prevY = tailY [0];
+
+int prev2X, prev2Y;
+
+tailX[0] = x;
+
+tailY[0] = y;
+
+for(int i = 1;i < nTail ; i++) {
+
+prev2X = tailX[i];
+
+prev2Y = tailY[i];
+
+tailX[i] = prevX;
+
+tailY[i] = prevY;
+
+prevX = prev2X;
+
+prevY = prev2Y ;
+
+}
+
+switch (dir) {
+
+case LEFT:
+
+x--;
+
+break;
+
+case RIGHT:
+
+x++;
+
+break;
+
+case UP:
+
+y--;
+
+break;
+
+case DOWN:
+
+y++;
+
+break;
+
+default:
+
+break;
+
+}
+
+if (x >= width) x =0;else if (x <0) x = width -1;
+
+if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+for (int i =0; i< nTail ;i++)
+
+if (tailX[i] == x && tailY[i] == y)
+gameover = true;
+
+if (x == fruitX && y == fruitY) {
+
+score +=10;
+
+fruitX = rand() % width;
+
+fruitY = rand() % height;
+
+nTail ++;
+
+}
+
+}
+
+int main()
+{
+
+Setup();
+
+while (!gameover) {
+
+Draw ();
+
+Input ();
+
+algorithm ();
+
+}
+
+return 0;
+
+}
